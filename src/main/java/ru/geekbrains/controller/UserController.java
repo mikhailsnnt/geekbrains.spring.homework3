43993ea7c2ec332a31a4be3c71d72cf569c3777c
Mihail Nikitin <nitinin.miha@icloud.com>
2022-04-05 22:24:47 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/controller/UserController.java
@@ -0,0 +1,47 @@
+package ru.geekbrains.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.geekbrains.persist.User;
+import ru.geekbrains.persist.UserRepository;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public String listPage(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "user";
+    }
+
+    @GetMapping("/{id}")
+    public String form(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userRepository.findById(id));
+        return "user_form";
+    }
+
+    @GetMapping("/new")
+    public String form(Model model) {
+        // TODO model.addAttribute("user", .........);
+        return "user_form";
+    }
+
+    @PostMapping
+    public String save(User user) {
+        userRepository.save(user);
+        return "redirect:/user";
+    }
+}
